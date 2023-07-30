08cf4fd82df5aff1dcb10b266ccc5b1b92d51c31
SerbanHiro <51057957+SerbanHiro@users.noreply.github.com>
2023-07-30 19:58:03 +0300

    1.3.0 metrics

--- src/main/java/me/serbob/toastedad/ToastedAD.java
+++ src/main/java/me/serbob/toastedad/ToastedAD.java
@@ -2,7 +2,9 @@ package me.serbob.toastedad;
 
 import jdk.jfr.internal.Logger;
 import me.serbob.toastedad.Commands.AD;
+import me.serbob.toastedad.Metrics.Metrics;
 import me.serbob.toastedad.TabCompleter.ADTC;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
@@ -11,6 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 public final class ToastedAD extends JavaPlugin {
     public static Map<Player,Integer> playerTime = new HashMap<>();
@@ -23,6 +26,7 @@ public final class ToastedAD extends JavaPlugin {
         getCommand("ad").setTabCompleter(new ADTC());
         registerPerms();
         startScheduler();
+        enableMetrics();
     }
     @Override
     public void onDisable() {}
@@ -47,5 +51,33 @@ public final class ToastedAD extends JavaPlugin {
                 playerTime.replace(entry.getKey(),--time);
             }
         }, 0L, 20L);
+    }
+    public void enableMetrics() {
+        Metrics metrics = new Metrics(this,19313);
+        metrics.addCustomChart(new Metrics.MultiLineChart("players_and_servers", new Callable<Map<String, Integer>>() {
+            @Override
+            public Map<String, Integer> call() throws Exception {
+                Map<String, Integer> valueMap = new HashMap<>();
+                valueMap.put("servers", 1);
+                valueMap.put("players", Bukkit.getOnlinePlayers().size());
+                return valueMap;
+            }
+        }));
+        metrics.addCustomChart(new Metrics.DrilldownPie("java_version", () -> {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+            String javaVersion = System.getProperty("java.version");
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(javaVersion, 1);
+            if (javaVersion.startsWith("1.7")) {
+                map.put("Java 1.7", entry);
+            } else if (javaVersion.startsWith("1.8")) {
+                map.put("Java 1.8", entry);
+            } else if (javaVersion.startsWith("1.9")) {
+                map.put("Java 1.9", entry);
+            } else {
+                map.put("Other", entry);
+            }
+            return map;
+        }));
     }
 }
